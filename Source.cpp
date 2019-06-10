b2cd92d00fcd81472742f5b3a4b1969c0bfbe2db
Kirill <45688082+ip-default-ttl@users.noreply.github.com>
2019-06-10 04:38:26 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+inline void message1();
+inline void message2();
+inline void main_help();
+inline void PascalABCnet();
+
 class pole
 {
 protected:
@@ -19,19 +24,45 @@ public:
         length = a;
         width = b;
         count = c;
-        tabl = new int *[length];
-        for (int i = 0; i < length; i++)
+        tabl = new int *[length + 2];
+        for (int i = 0; i < length + 2; i++)
         {
-            tabl[i] = new int[width];
+            tabl[i] = new int[width + 2];
         }
-        htabl = new int *[length];
-        for (int i = 0; i < length; i++)
+        htabl = new int *[length + 2];
+        for (int i = 0; i < length + 2; i++)
         {
-            htabl[i] = new int[width];
+            htabl[i] = new int[width + 2];
         }
-        mina = new bool[length * width];
+        for (int i = 1; i < length + 2; i++)
+        {
+            for (int j = 1; j < width + 2; j++)
+            {
+                tabl[i][j] = 9;
+            }
+        }
+        for (int i = 0; i < width + 2; i++)
+        {
+            tabl[0][i] = 228;
+            htabl[0][i] = 228;
+        }
+        for (int i = 0; i < length + 2; i++)
+        {
+            tabl[i][0] = 228;
+            htabl[i][0] = 228;
+        }
+        for (int i = 0; i < width + 2; i++)
+        {
+            tabl[length + 1][i] = 228;
+            htabl[length + 1][i] = 228;
+        }
+        for (int i = 0; i < length + 2; i++)
+        {
+            tabl[i][width + 1] = 228;
+            htabl[i][width + 1] = 228;
+        }
     }
-    void mines() //самый хреновый по времени алгоритм для этого, но ладно, сойдет
+    inline void mines() //самый хреновый по времени алгоритм для этого, но ладно, сойдет
     {
         int i = 0;
         int k = 0;
@@ -39,8 +70,8 @@ public:
         while (i < count)
         {
             srand(time(NULL));
-            k = rand() % length;
-            k1 = rand() % width;
+            k = 1 + rand() % length;
+            k1 = 1 + rand() % width;
             if (!(htabl[k][k1] == -1))
             {
                 htabl[k][k1] = -1;
@@ -48,32 +79,110 @@ public:
             }
         }
     }
-    void out()
+    inline void out()
     {
-        for (int i = 0; i < length; i++)
+        for (int i = 1; i < length + 1; i++)
         {
-            for (int j = 0; j < width; j++)
+            for (int j = 1; j < width + 1; j++)
             {
-                cout << htabl[i][j] << " ";
+                switch (tabl[i][j])
+                {
+                case 9:
+                {
+                    cout << "  #  ";
+                    break;
+                }
+                case 10:
+                {
+                    cout << "  ?  ";
+                    break;
+                }
+                case 11:
+                {
+                    cout << "  *  ";
+                    break;
+                }
+                default:
+                {
+                    cout << tabl[i][j] << "(" << htabl[i][j] << ") ";
+                }
+                }
             }
             cout << endl;
         }
     }
-    void calculate()
+    inline void calculate()
     {
-        for (int i = 1; i < (length - 2); i++)
+        for (int i = 1; i < length + 1; i++)
         {
-            if (htabl[i - 1][j - 1] == -1)
-                htabl[i][j]++;
-            if (htabl[i][j] == -1) (htabl[i][j]++)
+            for (int j = 1; j < width + 1; j++)
+            {
+                if (!(htabl[i][j] == -1))
+                {
+                    {
+                        if (htabl[i - 1][j - 1] == -1)
+                            htabl[i][j]++;
+                        if (htabl[i - 1][j] == -1)
+                        {
+                            htabl[i][j]++;
+                        }
+                        if (htabl[i - 1][j + 1] == -1)
+                        {
+                            htabl[i][j]++;
+                        }
+                        if (htabl[i][j - 1] == -1)
+                        {
+                            htabl[i][j]++;
+                        }
+                        if (htabl[i][j + 1] == -1)
+                        {
+                            htabl[i][j]++;
+                        }
+                        if (htabl[i + 1][j - 1] == -1)
+                        {
+                            htabl[i][j]++;
+                        }
+                        if (htabl[i + 1][j] == -1)
+                        {
+                            htabl[i][j]++;
+                        }
+                        if (htabl[i + 1][j + 1] == -1)
+                        {
+                            htabl[i][j]++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    inline void game()
+    {
+        string com;
+        bool win = false;
+        int d = 0;
+        int d1 = 0;
+        while (!(com == "stop"))
+        {
+            out();
+            cout << "game";
+            cin >> com;
+            if (com == "-m")
+            {
+            }
+            if (com == "-e")
+            {
+            }
+            if ((com == "--h") || (com == "help"))
+            {
+            }
+            if (com == "-h")
+            {
+                cin >> d >> d1;
+                tabl[d][d1] = 10;
+            }
         }
     }
 };
-
-inline void message1();
-inline void message2();
-inline void main_help();
-inline void PascalABCnet();
 
 int main()
 {
@@ -90,20 +199,18 @@ int main()
             int f1 = 0;
             int f2 = 0;
             cin >> f >> f1 >> f2;
-            pole A(8, 8, 20);
+            pole A(f, f1, f2);
             A.mines();
-            //A.calculate();
-            A.out();
-            string icommand;
-            while ((!(icommand == "stop")) || (win = false))
-            {
-                cout << "game>>>";
-                cin >> icommand;
-            }
+            A.calculate();
+            A.game();
         }
         if (command == "help")
         {
             main_help();
+        }
+        if (command == "PascalABCnet")
+        {
+            PascalABCnet();
         }
     }
     return 0;
@@ -132,6 +239,7 @@ inline void main_help()
     cout << "1) start - начало игры" << endl;
     cout << "2) PascalABCnet - избавит вас от мучений с Linux. Note: it is effective!" << endl;
     cout << "3) exit - выход из скрипта" << endl;
+    cout << "4) help - вывод меню помощи" << endl;
 }
 
 inline void PascalABCnet()
